140581a7415bda713e499f6a6c8600164808ff3c
Peter Sheats <sheats@gmail.com>
2020-04-11 18:49:33 -0400

    validate broker

--- tests/test_validator.py
+++ tests/test_validator.py
@@ -1,20 +1,57 @@
 import pytest
 
 from trade import Trade
-from validator import TradeValidator, InvalidSymbolException
+from validator import TradeValidator, InvalidSymbolException, InvalidBrokerException
 
 
-def test_valid_symbols():
-    validator = TradeValidator(symbols=["ABC"], brokers=[])
-    validator.validate_trade(Trade(symbol="ABC"))  # no error
+@pytest.fixture
+def valid_trade():
+    """For convenience return a validator and trade that raises no errors"""
+    validator = TradeValidator(symbols=["ABC", "XYZ"], brokers=["Broker 1", "Broker2"])
+
+    trade = Trade(symbol="ABC", broker="Broker 1")
+    return validator, trade
+
+
+def test_valid_symbols(valid_trade):
+    validator, trade = valid_trade
+    validator.validate_trade(trade)
+    trade.symbol = "AbC"
+    validator.validate_trade(trade)
+
     with pytest.raises(InvalidSymbolException):
         # Bad symbol
-        validator.validate_trade(Trade(symbol="DEF"))
+        trade.symbol = "DEF"
+        validator.validate_trade(trade)
 
     with pytest.raises(InvalidSymbolException):
         # No symbol
-        validator.validate_trade(Trade(symbol=None))
+        trade.symbol = None
+        validator.validate_trade(trade)
 
     with pytest.raises(InvalidSymbolException):
-        #
-        validator.validate_trade(Trade(symbol=1))
+        # Bad data
+        trade.symbol = 1
+        validator.validate_trade(trade)
+
+
+def test_valid_broker(valid_trade):
+    validator, trade = valid_trade
+    validator.validate_trade(trade)
+    trade.broker = "BROker2"
+    validator.validate_trade(trade)
+
+    with pytest.raises(InvalidBrokerException):
+        # No broker
+        trade.broker = None
+        validator.validate_trade(trade)
+
+    with pytest.raises(InvalidBrokerException):
+        # Empty
+        trade.broker = ""
+        validator.validate_trade(trade)
+
+    with pytest.raises(InvalidBrokerException):
+        # Not in the list
+        trade.broker = "Not in the list"
+        validator.validate_trade(trade)
--- validator.py
+++ validator.py
@@ -5,11 +5,19 @@ class InvalidSymbolException(Exception):
     pass
 
 
+class InvalidBrokerException(Exception):
+    pass
+
+
 class TradeValidator:
     def __init__(self, symbols, brokers):
-        self.symbols = symbols
-        self.brokers = brokers
+        self.symbols = [s.upper() for s in symbols]
+        self.brokers = [b.upper() for b in brokers]
 
     def validate_trade(self, trade):
-        if trade.symbol is None or trade.symbol.strip() not in self.symbols:
+
+        if not trade.symbol or str(trade.symbol).strip().upper() not in self.symbols:
             raise InvalidSymbolException()
+
+        if not trade.broker or str(trade.broker).strip().upper() not in self.brokers:
+            raise InvalidBrokerException
