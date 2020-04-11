28db470ea7418d880e22f80820182eaab330d7a2
Peter Sheats <sheats@gmail.com>
2020-04-11 17:52:19 -0400

    refactored loading code and loading trades

--- main.py
+++ main.py
@@ -1,34 +1,42 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import csv
+
+
+def _load_text_file(path):
+    items = []
+    with open(path, "r") as the_file:
+        for line in the_file:
+            items.append(line.strip())
+    return items
 
 
 def load_symbols():
-    symbols = []
-    with open("data/symbols.txt", "r") as the_file:
-        for line in the_file:
-            symbols.append(line.strip())
-    return symbols
+    return _load_text_file("data/symbols.txt")
 
 
 def load_brokers():
-    brokers = []
-    with open("data/firms.txt", "r") as the_file:
-        for line in the_file:
-            brokers.append(line.strip())
-    return brokers
+    return _load_text_file("data/firms.txt")
 
 
-def load_orders():
-    pass
+def load_trades():
+    trades = []
+    with open("data/trades.csv") as csv_file:
+        reader = csv.DictReader(csv_file)
+        for row in reader:
+            trades.append(row)
+    return trades
 
 
 def main():
     valid_symbols = load_symbols()
     permitted_brokers = load_brokers()
+    trades = load_trades()
     from pprint import pprint
 
     pprint(valid_symbols)
     pprint(permitted_brokers)
+    pprint(trades)
 
 
 if __name__ == "__main__":
